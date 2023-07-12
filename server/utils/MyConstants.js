a616587c1be919b6ad9b75b155658c44bd866361
hoangvi241002 <126341547+hoangvi241002@users.noreply.github.com>
2023-07-13 00:28:59 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster.il8uxha.mongodb.net',
+    DB_USER: 'locnguyen5307',
+    DB_PASS: 'tanloc0507141102',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'hoangvi080@gmail.com',
+    EMAIL_PASS: 'xwbideaezapdvtgn',
+    JWT_SECRET: '123',
+    JWWT_EXPIRES: '100000000000000000000000000',
+};
+module.exports = MyConstants;
